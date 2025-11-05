a6f83350ec6fb0068fa71630ddbd39159f4469ab
moven0831 <moven0831@gmail.com>
2025-11-05 18:04:33 -0500

    chore: migrate wallet-poc from https://github.com/privacy-ethereum/zkID/commit/d57c6d362fd6fed95a0413eb211933dc5830d511

--- wallet-unit-poc/ecdsa-spartan2/build.rs
+++ wallet-unit-poc/ecdsa-spartan2/build.rs
@@ -1,10 +1,7 @@
 use rust_witness::transpile::transpile_wasm;
 
 fn main() {
-    // Transpile WASM files from the circom build directory to C
-    // This will transpile both ECDSA and JWT circuits
+    // // Transpile WASM files from the circom build directory to C
+    // // This will transpile both ECDSA and JWT circuits
     transpile_wasm("../circom/build/".to_string());
-
-    // Tell cargo to link the circuit library
-    println!("cargo:rustc-link-lib=static=circuit");
 }
